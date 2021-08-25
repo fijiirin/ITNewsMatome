b976c5e5b2bf6a81d2542b37e5a2e5f166616dd4
“藤井 <204003@himejo.jp>
2021-08-25 10:57:14 +0900

    add
    SegementSlideでタブの実装, 記事を表示するtableViewの作成

---
+++ ITNewsMatome/Controller/ArticleViewController.swift
@@ -0,0 +1,148 @@
+//
+//  NewsViewController.swift
+//  ITNewsMatome
+//
+//  Created by 藤井凜 on 2021/08/24.
+//
+
+import UIKit
+import SegementSlide
+
+class ArticleViewController: UITableViewController, SegementSlideContentScrollViewDelegate, XMLParserDelegate {
+  
+  
+  var parser = XMLParser()
+  
+  var currentElementName:String!
+  
+  var articleItemArray = [ArticleItem]()
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    tableView.backgroundColor = .clear
+    
+    //startParser(url: URL(string: "")!)
+  }
+  
+  
+  func startParser(url: URL) {
+    
+    parser = XMLParser(contentsOf: url)!
+    parser.delegate = self
+    parser.parse()
+  }
+  
+  
+  @objc var scrollView: UIScrollView {
+    return tableView
+  }
+  
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 0
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return 0
+  }
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    
+    
+    return cell
+  }
+  
+  func parser(_ parser: XMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String] = [:]) {
+    
+    currentElementName = nil
+    
+    if elementName == "item" {
+      
+      self.articleItemArray.append(ArticleItem())
+    } else {
+      currentElementName = elementName
+    }
+  }
+  
+  func parser(_ parser: XMLParser, foundCharacters string: String) {
+    
+    if self.articleItemArray.count > 0 {
+      
+      var lastItem = self.articleItemArray[self.articleItemArray.count - 1]
+      
+      switch self.currentElementName {
+      case "title":
+        lastItem.title = string
+        print(string)
+      case "link":
+        lastItem.url = string
+      case "pubDate":
+        lastItem.date = string
+      default:
+        break
+      }
+    }
+  }
+  
+  func parser(_ parser: XMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+    self.currentElementName = nil
+  }
+  
+  func parserDidEndDocument(_ parser: XMLParser) {
+    tableView.reloadData()
+  }
+  
+  
+  /*
+   // Override to support conditional editing of the table view.
+   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the specified item to be editable.
+   return true
+   }
+   */
+  
+  /*
+   // Override to support editing the table view.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+   if editingStyle == .delete {
+   // Delete the row from the data source
+   tableView.deleteRows(at: [indexPath], with: .fade)
+   } else if editingStyle == .insert {
+   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+   }
+   }
+   */
+  
+  /*
+   // Override to support rearranging the table view.
+   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+   
+   }
+   */
+  
+  /*
+   // Override to support conditional rearranging of the table view.
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the item to be re-orderable.
+   return true
+   }
+   */
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
+}
---
+++ ITNewsMatome/Controller/BaseViewController.swift
@@ -0,0 +1,80 @@
+//
+//  BaseViewController.swift
+//  ITNewsMatome
+//
+//  Created by 藤井凜 on 2021/08/24.
+//
+
+import UIKit
+import Firebase
+import SegementSlide
+
+class BaseViewController: SegementSlideDefaultViewController {
+  
+  
+  override func viewDidLoad() {
+    
+    super.viewDidLoad()
+    
+    Auth.auth().signInAnonymously(completion: nil)
+    
+    reloadData()
+    
+    defaultSelectedIndex = 0
+  }
+  
+  
+//  override func segementSlideHeaderView() -> UIView {
+//
+//    let headerView = UIImageView()
+//
+//    headerView.isUserInteractionEnabled = true
+//    headerView.contentMode = .scaleAspectFill
+//    headerView.image = UIImage(named: "header")
+//    headerView.translatesAutoresizingMaskIntoConstraints = false
+//
+//    let headerHeight: CGFloat
+//
+//    if #available(iOS 11.0, *) {
+//
+//      headerHeight = view.bounds.height/4+view.safeAreaInsets.top
+//
+//    } else {
+//
+//      headerHeight = view.bounds.height/4+topLayoutGuide.length
+//
+//    }
+//
+//    headerView.heightAnchor.constraint(equalToConstant: headerHeight).isActive = true
+//
+//    return headerView
+//  }
+  
+  override var titlesInSwitcher: [String] {
+
+    return ["トップ","トーク","お気に入り","追加要望","お問い合わせ"]
+  }
+  
+  
+  override func segementSlideContentViewController(at index: Int) -> SegementSlideContentScrollViewDelegate? {
+    
+    switch index {
+    
+    case 0:
+      return ArticleViewController()
+    case 1:
+      return TalkViewController()
+    case 2:
+      return FavoriteViewController()
+    case 3:
+      return RequestViewController()
+    case 4:
+      return ContactViewController()
+    default:
+      return ArticleViewController()
+    }
+  }
+  
+  
+  
+}

835da2613c37b8008eece6f5f490c19f0fe6718e
“藤井 <204003@himejo.jp>
2021-09-06 07:57:38 +0900

    add
    メッセージ送信機能の作成

--- ITNewsMatome/Controller/createTalkViewController.swift
+++ ITNewsMatome/Controller/createTalkViewController.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import Firebase
 
 class CreateTalkViewController: UIViewController {
   
   @IBOutlet weak var nameTextField: UITextField!
   @IBOutlet weak var messageTextView: PlaceHolderTextView!
   
-  
+  let sendDBModel = SendDBModel()
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -21,15 +22,11 @@ class CreateTalkViewController: UIViewController {
     nameTextField.becomeFirstResponder()
   }
   
+  @IBAction func send(_ sender: Any) {
+    
+    sendDBModel.sendTalk(name: nameTextField.text!, message: messageTextView.text!, userID: Auth.auth().currentUser!.uid)
+  }
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  
   
 }
--- ITNewsMatome/Model/SendDBModel.swift
+++ ITNewsMatome/Model/SendDBModel.swift
@@ -6,9 +6,20 @@
 //
 
 import Foundation
-
+import Firebase
 
 class SendDBModel {
+  
+  
+  let db = Firestore.firestore()
+  
+  func sendTalk(name:String, message:String, userID:String) {
+    
+    
+    db.collection("Talks").document().setData(
+      ["name":name as Any, "message":message as Any, "userID":userID as Any, "date":Date().timeIntervalSince1970]
+    )
+  }
   
   
 }
